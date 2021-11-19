cc17906c419d94e0befe68a4c4a4d4219822d9be
efadini <53356489+efadini@users.noreply.github.com>
2021-11-18 22:06:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,557 @@
+let hexCode = [
+    {
+        color: "indian red",
+        code: {
+            hex: "#B0171F"
+        },
+        id: 1
+    },
+
+    {
+        color: "crimson",
+        code: {
+            hex: "#DC143C"
+        },
+        id: 2
+    },
+
+    {
+        color: "lightpink",
+        code: {
+            hex: "#FFB6C1"
+        },
+        id: 4
+    },
+
+    {
+        color: "pink",
+        code: {
+            hex: "#FFC0CB"
+        },
+        id: 8
+    },
+
+    {
+        color: "palevioletred",
+        code: {
+            hex: "#DB7093"
+        },
+        id: 13
+    },
+
+    {
+        color: "lavenderblush",
+        code: {
+            hex: "#FFF0F5"
+        },
+        id: 18
+    },
+
+    {
+        color: "violetred",
+        code: {
+            hex: "#FF3E96"
+        },
+        id: 22
+    },
+
+    {
+        color: "hotpink",
+        code: {
+            hex: "#FF69B4"
+        },
+        id: 26
+    },
+   
+    {
+        color: "raspberry",
+        code: {
+            hex: "#872657"
+        },
+        id: 31
+    },
+
+    {
+        color: "deeppink",
+        code: {
+            hex: "#FF1493"
+        },
+        id: 32
+    },
+
+    {
+        color: "maroon",
+        code: {
+            hex: "#FF34B3"
+        },
+        id: 36
+    },
+
+    {
+        color: "mediumvioletred",
+        code: {
+            hex: "#C71585"
+        },
+        id: 39
+    },
+
+    {
+        color: "violetred",
+        code: {
+            hex: "#D02090"
+        },
+        id: 40
+    },
+
+    {
+        color: "orchid",
+        code: {
+            hex: "#DA70D6"
+        },
+        id: 41
+    },
+
+    {
+        color: "thistle",
+        code: {
+            hex: "#D8BFD8"
+        },
+        id: 46
+    },
+
+    {
+        color: "plum",
+        code: {
+            hex: "#DDA0DD"
+        },
+        id: 55
+    },
+
+    {
+        color: "violet",
+        code: {
+            hex: "#EE82EE"
+        },
+        id: 56
+    },
+
+    {
+        color: "magenta",
+        code: {
+            hex: "#FF00FF"
+        },
+        id: 57
+    },
+
+    {
+        color: "purple*",
+        code: {
+            hex: "#800080"
+        },
+        id: 61
+    },
+
+    {
+        color: "mediumorchid",
+        code: {
+            hex: "#BA55D3"
+        },
+        id: 62
+    },
+
+    {
+        color: "darkviolet",
+        code: {
+            hex: "#9400D3"
+        },
+        id: 67
+    },
+
+    {
+        color: "darkorchid",
+        code: {
+            hex: "#9932CC"
+        },
+        id: 68
+    },
+
+    {
+        color: "indigo",
+        code: {
+            hex: "#4B0082"
+        },
+        id: 73
+    },
+
+    {
+        color: "blueviolet",
+        code: {
+            hex: "#8A2BE2"
+        },
+        id: 74
+    },
+
+    {
+        color: "purple",
+        code: {
+            hex: "#9B30FF"
+        },
+        id: 75
+    },
+
+    {
+        color: "mediumpurple",
+        code: {
+            hex: "#9370DB"
+        },
+        id: 79
+    },
+
+    {
+        color: "darkslateblue",
+        code: {
+            hex: "#483D8B"
+        },
+        id: 84
+    },
+
+    {
+        color: "lightslateblue",
+        code: {
+            hex: "#8470FF"
+        },
+        id: 85
+    },
+
+    {
+        color: "mediumslateblue",
+        code: {
+            hex: "#7B68EE"
+        },
+        id: 86
+    },
+
+    {
+        color: "slateblue",
+        code: {
+            hex: "#6A5ACD"
+        },
+        id: 87
+    },
+
+    {
+        color: "ghostwhite",
+        code: {
+            hex: "#F8F8FF"
+        },
+        id: 92
+    },
+
+    {
+        color: "lavender",
+        code: {
+            hex: "#E6E6FA"
+        },
+        id: 93
+    },
+
+    {
+        color: "blue",
+        code: {
+            hex: "#0000FF"
+        },
+        id: 94
+    },
+
+    {
+        color: "mediumblue",
+        code: {
+            hex: "#0000CD"
+        },
+        id: 96
+    },
+
+    {
+        color: "darkblue",
+        code: {
+            hex: "#00008B"
+        },
+        id: 97
+    },
+
+    {
+        color: "navy",
+        code: {
+            hex: "#000080"
+        },
+        id: 98
+    },
+
+    {
+        color: "midnightblue",
+        code: {
+            hex: "#191970"
+        },
+        id: 99
+    },
+
+    {
+        color: "cobalt",
+        code: {
+            hex: "#3D59AB"
+        },
+        id: 100
+    },
+
+    {
+        color: "royalblue",
+        code: {
+            hex: "#4169E1"
+        },
+        id: 101
+    },
+    {
+        color: "cornflowerblue",
+        code: {
+            hex: "#6495ED"
+        },
+        id: 106
+    },
+
+    {
+        color: "lightsteelblue",
+        code: {
+            hex: "#B0C4DE"
+        },
+        id: 107
+    },
+
+
+    {
+        color: "lightslategray",
+        code: {
+            hex: "#778899"
+        },
+        id: 112
+    },
+
+    {
+        color: "slategray",
+        code: {
+            hex: "#708090"
+        },
+        id: 113
+    },
+
+
+    {
+        color: "dodgerblue",
+        code: {
+            hex: "#1E90FF"
+        },
+        id: 118
+    },
+
+    {
+        color: "aliceblue",
+        code: {
+            hex: "#F0F8FF"
+        },
+        id: 122
+    },
+
+    {
+        color: "steelblue",
+        code: {
+            hex: "#4682B4"
+        },
+        id: 123
+    },
+
+    {
+        color: "lightskyblue",
+        code: {
+            hex: "#87CEFA"
+        },
+        id: 128
+    },
+
+
+    {
+        color: "skyblue",
+        code: {
+            hex: "#87CEFF"
+        },
+        id: 133
+    },
+
+    {
+        color: "deepskyblue",
+        code: {
+            hex: "#00BFFF"
+        },
+        id: 138
+    },
+
+    {
+        color: "peacock",
+        code: {
+            hex: "#33A1C9"
+        },
+        id: 142
+    },
+
+    {
+        color: "lightblue",
+        code: {
+            hex: "#ADD8E6"
+        },
+        id: 143
+    },
+
+
+
+    {
+        color: "powderblue",
+        code: {
+            hex: "#B0E0E6"
+        },
+        id: 148
+    },
+
+    {
+        color: "cadetblue",
+        code: {
+            hex: "#98F5FF"
+        },
+        id: 149
+    },
+
+
+    {
+        color: "turquoise",
+        code: {
+            hex: "#00F5FF"
+        },
+        id: 153
+    },
+
+
+    {
+        color: "cadetblue",
+        code: {
+            hex: "#5F9EA0"
+        },
+        id: 157
+    },
+
+    {
+        color: "darkturquoise",
+        code: {
+            hex: "#00CED1"
+        },
+        id: 158
+    },
+
+    {
+        color: "azure",
+        code: {
+            hex: "#F0FFFF"
+        },
+        id: 159
+    },
+
+    {
+        color: "lightcyan",
+        code: {
+            hex: "#E0FFFF"
+        },
+        id: 163
+    }
+]
+
+function randomIndexHex(min, max) {    
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return hexCode[Math.floor(Math.random() * (max - min)) + min];
+}
+
+function randomIndexAux(min, max, vetorAux) {    
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return vetorAux[Math.floor(Math.random() * (max - min)) + min];
+}
+
+function randomNumber(cores) {
+    vetor = [];
+    var quantidade = 5;
+    aux = 0;
+    for (var i = 0; i < quantidade; i++) {
+        r = randomIndexHex(0, cores.length);
+        if (vetor.length != 0 && vetor.includes(r)) {
+            do {
+                aux2 = randomIndexHex(0, cores.length);
+                console.log(aux2);
+            } while (vetor.includes(aux2));
+            vetor[aux] = aux2;
+            aux = aux + 1;
+        } else {
+            vetor[aux] = r;
+            aux = aux + 1;
+        }
+    }
+    return vetor;
+}
+
+function escolher(cores) {
+
+    vetor = [];
+    var fim = false;
+    var vetorAux = randomNumber(cores);
+    var regex = /^[a-zA-Z\s]*$/
+    index = randomIndexAux(0, vetorAux.length, vetorAux);
+    corEscolhida = index.color;
+    corEscolhidaHex = index.code.hex;
+    vetorCores = getCoresNomes(vetorAux)
+
+    //alert(corEscolhida);
+
+    do {
+        var cor = prompt(`Eu estou pensando em uma dessas cores: \n\n ${vetorCores} \n\nEm qual cor estou pensado?`);
+        if (cor.match(regex)) {
+            if (cor.match(corEscolhida)) {
+                fim = true;
+                document.body.style.backgroundColor = corEscolhidaHex;
+                alert("Parábens você acertou!!!");
+            } else {
+                if (cor.substr(0, 1) > corEscolhida.substr(0, 1)) {
+                    alert("A cor escolhida é alfabeticamente menor");
+                } else if (cor.substr(0, 1) < corEscolhida.substr(0, 1)) {
+                    alert("A cor escolhida é alfabeticamente maior");
+                } else if (cor.substr(0, 1) == corEscolhida.substr(0, 1)) {
+                    alert("A cor escolhida é alfabeticamente igual");
+                }
+            }
+        } else {
+            alert("Inválido");
+        }
+    } while (fim == false)
+}
+
+function getCoresNomes(data) {
+    vetor = [data.length]
+    for (var i = 0; i < data.length; i++) {
+        vetor[i] = data[i].color;
+    }
+    return vetor
+}
+
+function getCoresHex(data) {
+    vetor = [data.length]
+    for (var i = 0; i < data.length; i++) {
+        vetor[i] = data[i].code.hex;
+    }
+    return vetor
+}
+
+escolher(hexCode);
